9594b162029dba008cd3b7f83010aca912043140
milli <million.fil@mail.ru>
2019-12-27 16:52:45 +0500

    Доделаны все сортировки

--- src/main/java/springApi/Application.java
+++ src/main/java/springApi/Application.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+    //TODO Сделать добавление данных
+    //TODO Сделать удаление данных
+    //TODO Сделать редактирование данных
+    //TODO Сделать uTest
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
